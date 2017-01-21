0bdb2fa7a976f137254e2bcc8aaa2bcffe5f37cb
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-01-21 11:20:22 +0900

    Add test cases

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 # encoding: utf-8
 
+import os.path
+import sys
 
 from distutils.core import setup
+import setuptools
+
+
+REQUIREMENT_DIR = "requirements"
+
+needs_pytest = set(["pytest", "test", "ptr"]).intersection(sys.argv)
+pytest_runner = ["pytest-runner"] if needs_pytest else []
+
+
+with open(os.path.join(REQUIREMENT_DIR, "requirements.txt")) as f:
+    install_requires = [line.strip() for line in f if line.strip()]
+
+with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
+    tests_require = [line.strip() for line in f if line.strip()]
 
 setup(
     name='allpairspy',
@@ -15,7 +31,10 @@ into a lesser set that covers most situations.
     author="Tsuyoshi Hombashi",
     author_email="gogogo.vm@gmail.com",
     url='https://github.com/thombashi/allpairspy',
-    packages=['metacomm', 'metacomm.combinatorics'],
+    install_requires=install_requires,
+    tests_require=tests_require,
+    setup_requires=pytest_runner,
+    packages=setuptools.find_packages(exclude=["test*"]),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
