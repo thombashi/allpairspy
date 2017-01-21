71ef0047efef57cb9d90a45e7fbe8237ba8f1f54
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-01-21 13:20:37 +0900

    Rename a directory

--- allpairspy/combinatorics/combinatorics.py
+++ allpairspy/combinatorics/combinatorics.py
@@ -12,6 +12,6 @@ def xuniqueCombinations(items, n):
     if n == 0:
         yield []
     else:
-        for i in range(len(items)):
+        for i, item in enumerate(items):
             for cc in xuniqueCombinations(items[i + 1:], n - 1):
-                yield [items[i]] + cc
+                yield [item] + cc
