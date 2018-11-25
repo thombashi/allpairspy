32a5e3888c01800956018bad80ebcb218b5195b8
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-11-25 14:14:37 +0900

    Fix coveralls

--- .travis/run_test.sh
+++ .travis/run_test.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-pyver=$(python -c "from __future__ import print_function; import sys; print('{}{}'.format(*sys.version_info[0:2]))")
-
-tox
-
-if [ "$TRAVIS_OS_NAME" = "linux" ] && [ "$pyver" = "36" ]; then
-    python setup.py test --addopts "-v --cov allpairspy --cov-report term-missing"
+if [ "$TOXENV" != "cov" ] ; then
+    tox
 fi
